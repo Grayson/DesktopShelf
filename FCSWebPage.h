d3e64598e4310e14a1c6cf3038de4720a41b0352
Grayson Hansard <info@fromconcentratesoftware.com>
2011-04-20 14:06:26 -0400

    Commit before going to github (private).

---
+++ FCSWebPage.h
@@ -0,0 +1,17 @@
+//
+//  FCSWebPage.h
+//  DesktopShelf
+//
+//  Created by Grayson Hansard on 4/12/11.
+//  Copyright 2011 From Concentrate Software. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface FCSWebPage : FCSWebPage {
+@private
+    
+}
+
+@end
---
+++ FCSWebPage.m
@@ -0,0 +1,29 @@
+//
+//  FCSWebPage.m
+//  DesktopShelf
+//
+//  Created by Grayson Hansard on 4/12/11.
+//  Copyright 2011 From Concentrate Software. All rights reserved.
+//
+
+#import "FCSWebPage.h"
+
+
+@implementation FCSWebPage
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+- (void)dealloc
+{
+    [super dealloc];
+}
+
+@end
